28c0e51ddeefd052a50a610db0e4bb820de6256e
Abhay <abhaypvt2903@gmail.com>
2026-01-01 15:19:19 +0000

    dom selector learning

---
+++ Dom.js
@@ -0,0 +1,14 @@
+function validateForm(){
+    alert("Form Submitted!")
+}
+
+var b=10
+console.log(b)
+
+document.querySelector("button").addEventListener("click", function(){
+alert("Form Submitted!")
+});
+
+document.querySelector(".arrow").addEventListener("click", function(){
+    window.scrollTo({top:0}); 
+});
